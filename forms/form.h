3dac99ca706c88ecef81bb3e7808dbddcb68e9d3
*(this)-programmer == i++ <140750964+this-progammer@users.noreply.github.com>
2026-03-16 10:20:55 -0500

    Update form.h

--- forms/form.h
+++ forms/form.h
@@ -3,6 +3,29 @@
 #ifndef FORM_
 #define FORM_
 
+#define BUTTON 0
+#define TOGGLE_BUTTON 1
+#define ICON_BUTTON 2
+#define MENU 3
+#define SUBMENU 4
+#define CONTEXT_STRIP 5
+#define PANEL 6
+#define FRAME 7
+#define LABEL 8
+#define TEXTBOX 9
+#define CHECKBOX 10
+#define RATIOBOX 11
+#define SPLITTER 12
+#define SCROLLBAR 13
+#define TOOLBAR 14
+#define IMAGEBOX 15
+#define LISTBOX 16
+#define TREELIST 17
+#define DYNAMICSTRING 18
+#define STATICSTRING 19
+#define MESSAGEBOX 20
+#define IMAGEPANEL 21
+
 namespace wfsforms
 {
   namespace component
